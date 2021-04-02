80cb2c1405d01215bff1a393cc0e1bf4a70b3ba0
Caio Braga <kaio.braga98@gmail.com>
2021-04-01 21:46:31 -0300

    Criando métodos na classe automato

--- AutomatonSimulator/Automato.cs
+++ AutomatonSimulator/Automato.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,24 @@ namespace AutomatonSimulator
 {
     public class Automato
     {
+        private ArrayList alfabeto;
+        private ArrayList estados;
+        public Estado estadoinicial { get; set; }
+        public Estado estadoFinal { get; set; }
+
+        public Automato()
+        {
+        }
+
+        public void addEstadoInicial(Estado estado)
+        {
+            estadoinicial = estado;
+            estados.Add(estado);
+        }
+
+        public void addEstado(Estado estado)
+        {
+            estados.Add(estado);
+        }
     }
 }
