e4612694668d39472688a2dcf103aeea712ec5d2
JvmeloO <jvmelo100@hotmail.com>
2021-04-03 23:11:33 -0300

    Realizando mais testes

--- AutomatonSimulator/Program.cs
+++ AutomatonSimulator/Program.cs
@@ -6,15 +6,26 @@ namespace AutomatonSimulator
     {
         private static void Main(string[] args)
         {
+            Estado q0 = new Estado("q0");
             Estado q1 = new Estado("q1");
             Estado q2 = new Estado("q2");
-            Transicao t = new Transicao("A", q2);           
-            q1.AdicionarTransicao(t);
+            Estado q3 = new Estado("q3");
             Automato automato = new Automato();
-            automato.AddEstadoStart(q1);
+            automato.AddEstadoStart(q0);
             automato.AddEstadoEnd(q2);
+            // TESTE: Aceitar todo alfabeto que comece com B e termine com A
+            q0.AdicionarTransicao(new Transicao("A", q3));
+            q0.AdicionarTransicao(new Transicao("B", q1));
+            q1.AdicionarTransicao(new Transicao("B", q1));
+            q1.AdicionarTransicao(new Transicao("A", q2));
+            q2.AdicionarTransicao(new Transicao("B", q1));
+            q2.AdicionarTransicao(new Transicao("A", q2));
+            automato.InserirLetrasAlfabeto("B");
             automato.InserirLetrasAlfabeto("A");
-            automato.VerificarAutomatoAceito();
+            automato.InserirLetrasAlfabeto("B");
+            automato.InserirLetrasAlfabeto("A");
+            automato.InserirLetrasAlfabeto("A");
+            Console.WriteLine(automato.VerificarAutomatoAceito());
         }
     }
 }
